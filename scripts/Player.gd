a2a6a578ab6b9aa0c0001dcceac926305d87bee7
Doc <renan_ota@hotmail.com>
2018-11-27 00:26:47 -0200

    Preparo de código para animações

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -13,14 +13,22 @@ func _physics_process(delta):
 	# Movimentos Básicos
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
+		$Sprite.flip_h = false;
+		#$Spirte.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
+		$Sprite.flip_h = true;
+		#$Spirte.play("run")
 	else:
 		motion.x = 0
+		#$Spirte.play("idle")
 	
 	# Pulo
 	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		motion.y = -JUMP_HEIGTH
+	else:
+		#$Sprite.play("jump")
+		pass
 	
 	# Movimentar
 	motion = move_and_slide(motion, UP)
