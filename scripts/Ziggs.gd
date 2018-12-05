0125dd4e822e0c8cfede79b4303112d3d43cd758
Doc <renan_ota@hotmail.com>
2018-12-04 23:54:09 -0200

    Cenario basico de testes

--- scripts/Ziggs.gd
+++ scripts/Ziggs.gd
@@ -1,26 +1,30 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const GRAVITY = 10
-const SPEED = 200
-const JUMP_HEIGTH = 300
+const GRAVITY = 20
+const SPEED = 300
+const JUMP_HEIGTH = 600
 onready var animation = get_node("../AnimationZiggs")
 var motion = Vector2()
 
 func _physics_process(delta):
 	# Aplicar gravidade
-	#motion.y += GRAVITY
+	motion.y += GRAVITY
 	
 	# Movimentos Básicos
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
+		$Torso.set_scale(Vector2(1,1))
 		if not animation.is_playing():
-			animation.play("idle")
+			#animation.play("floating")
+			pass
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
+		$Torso.set_scale(Vector2(-1,1))
 	else:
 		motion.x = 0
-		animation.stop()
+		if not animation.is_playing():
+			animation.play("idle")
 	
 	# Pulo
 	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
